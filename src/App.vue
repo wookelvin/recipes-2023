e2666949b4296b3d893a498e17134a3b39aaaf50
Kelvin Woo <kelvin.woo@gmail.com>
2023-06-22 22:51:18 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <metainfo>
+    <template #title="{ content }">
+      {{ content ? `${content} | SITE_NAME` : `SITE_NAME` }}
+    </template>
+  </metainfo>
+  <Container>
+    <RouterView />
+  </Container>
+</template>
+
+<style scoped lang="postcss"></style>
